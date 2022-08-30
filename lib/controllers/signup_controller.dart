b1ef453314c88e4bb62430f4295575211b38bb46
BS983 <shuvo1997@yahoo.com>
2022-08-30 18:45:00 +0600

    Separeted textController

--- lib/controllers/signup_controller.dart
+++ lib/controllers/signup_controller.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import '../constants/field_lables.dart';
+
 class SignUpFormController extends GetxController {
   RxBool isChecked = RxBool(false);
 
@@ -9,6 +11,22 @@ class SignUpFormController extends GetxController {
   String? dropDownValidation(String? value) {
     if (value == null) {
       return 'Please select a value';
+    }
+    return null;
+  }
+
+  String? textFieldValidation(String? value, String label) {
+    if (label == FieldLabel.mobileFieldLabel &&
+        (value == null || !value.isPhoneNumber)) {
+      return 'Please enter phone number';
+    }
+    if (label == FieldLabel.emailFieldLabel &&
+        (value == null || !value.isEmail)) {
+      return 'Please enter email address';
+    }
+    if (label == FieldLabel.referralFieldLabel &&
+        (value == null || !value.isNumericOnly)) {
+      return 'Please enter referral code';
     }
     return null;
   }
--- lib/widgets/text_field.dart
+++ lib/widgets/text_field.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:sign_up/constants/field_lables.dart';
+import 'package:sign_up/controllers/signup_controller.dart';
 
 class CustomTextField extends StatelessWidget {
   final String label;
@@ -15,51 +15,15 @@ class CustomTextField extends StatelessWidget {
       required this.onSaved})
       : super(key: key);
 
-  // void saveData() {
-  //   if (label == FieldLabel.mobileFieldLabel) {
-  //     for (var element in FieldList.fields) {
-  //       if (element.labelName == label) {
-  //         element.fieldValue = controller.text;
-  //       }
-  //     }
-  //     // FieldList.fields[2].fieldValue = controller.text;
-  //   }
-  //   if (label == FieldLabel.emailFieldLabel) {
-  //     for (var element in FieldList.fields) {
-  //       if (element.labelName == label) {
-  //         element.fieldValue = controller.text;
-  //       }
-  //     }
-  //   }
-  //   if (label == FieldLabel.referralFieldLabel) {
-  //     for (var element in FieldList.fields) {
-  //       if (element.labelName == label) {
-  //         element.fieldValue = controller.text;
-  //       }
-  //     }
-  //   }
-  // }
-
   @override
   Widget build(BuildContext context) {
+    SignUpFormController formController = Get.find<SignUpFormController>();
     return Padding(
         padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 5),
         child: TextFormField(
           autovalidateMode: AutovalidateMode.onUserInteraction,
           validator: (value) {
-            if (label == FieldLabel.mobileFieldLabel &&
-                (value == null || !value.isPhoneNumber)) {
-              return 'Please enter phone number';
-            }
-            if (label == FieldLabel.emailFieldLabel &&
-                (value == null || !value.isEmail)) {
-              return 'Please enter email address';
-            }
-            if (label == FieldLabel.referralFieldLabel &&
-                (value == null || !value.isNumericOnly)) {
-              return 'Please enter referral code';
-            }
-            return null;
+            return formController.textFieldValidation(value, label);
           },
           keyboardType: keyboardType,
           textInputAction: textInputAction,
